c590f6764e0e65986c8aa1030121bfaac4e66a67
ryan-b-writin <ryan.bliton@gmail.com>
2019-10-12 20:28:25 -0700

    add MoreThreads class

--- src/MultithreadingTutorial/MoreThreads.java
+++ src/MultithreadingTutorial/MoreThreads.java
@@ -3,7 +3,22 @@ package MultithreadingTutorial;
 public class MoreThreads {
 
 	public static void main(String[] args) {
-		System.out.println("print");
+		System.out.println("main thread starting");
+		
+		MyThread mt1 = MyThread.createAndStart("child #1");
+		MyThread mt2 = MyThread.createAndStart("child #2");
+		MyThread mt3 = MyThread.createAndStart("child #3");
+		
+		for (int i=0; i < 50 ; i++) {
+			System.out.println(".");
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException exc) {
+				System.out.println("Main thread interrupted");
+			}
+		}
+		
+		System.out.println("Main thread ending");
 
 	}
 
